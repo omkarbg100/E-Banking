830ddb060995e0966af277f46adb2935be1e1c86
omkar gaikwad <147027748+omkarbg100@users.noreply.github.com>
2026-01-06 20:32:10 +0530

    .env File For Frontend ,
    This is done so that i can host the Website and also can do Develpoment along with sharing the Website with people

--- Frontend/src/api/auth.api.js
+++ Frontend/src/api/auth.api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+//const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 /* ================= TOKEN HELPER ================= */
 const getAuthHeaders = () => {
